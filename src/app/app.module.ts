2531cb8d00222a8d45fd60cbdc5bfd7990bce73c
Alejandro Quadri <alejandroquadri@gmail.com>
2016-11-08 16:13:19 -0300

    Agregado configuracion para inicializar Firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,36 @@
 import { NgModule } from '@angular/core';
 import { IonicApp, IonicModule } from 'ionic-angular';
 import { MyApp } from './app.component';
+
+// paginas importadas
 import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
+import { LoginPage } from '../pages/login/login';
+import { ResetPasswordPage } from '../pages/reset-password/reset-password';
+import { SignupPage } from '../pages/signup/signup';
+
+// providers importados
+import { AuthData } from '../providers/auth-data';
+
+// importo AngularFire2 module
+import { AngularFireModule, AuthProviders, AuthMethods } from 'angularfire2';
+
+// settings AF2
+export const firebaseConfig = {
+  apiKey: "AIzaSyC9EFx_1rQDM0YOleEC-CstB58D0JMj0pA",
+  authDomain: "dietapp-9f200.firebaseapp.com",
+  databaseURL: "https://dietapp-9f200.firebaseio.com",
+  storageBucket: "dietapp-9f200.appspot.com",
+  messagingSenderId: "1075458661299"
+};
+
+// esto le dice a AF2 que voy a usar Email & Password para autenticacion
+const myFirebaseAuthConfig = {
+  provider: AuthProviders.Password,
+  method: AuthMethods.Password
+}
 
 @NgModule({
   declarations: [
@@ -12,10 +38,14 @@ import { TabsPage } from '../pages/tabs/tabs';
     AboutPage,
     ContactPage,
     HomePage,
-    TabsPage
+    TabsPage,
+    LoginPage,
+    ResetPasswordPage,
+    SignupPage
   ],
   imports: [
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig, myFirebaseAuthConfig)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -23,8 +53,13 @@ import { TabsPage } from '../pages/tabs/tabs';
     AboutPage,
     ContactPage,
     HomePage,
-    TabsPage
+    TabsPage,
+    LoginPage,
+    ResetPasswordPage,
+    SignupPage
   ],
-  providers: []
+  providers: [
+    AuthData
+  ]
 })
 export class AppModule {}
